fbefbbc9620130fcce3191c29f4f98f196f93abf
ton-oliveira <87735990+ton-oliveira@users.noreply.github.com>
2021-10-04 19:20:47 -0300

    Estimate the number of servings

--- coffee_machine.py
+++ coffee_machine.py
@@ -1,16 +1,32 @@
-print('''
-Starting to make a coffee
-Grinding coffee beans
-Boiling water
-Mixing boiled water with crushed coffee beans
-Pouring coffee into the cup
-Pouring some milk into the cup
-Coffee is ready!
-''')
+def qtd_water(aqua):
+    return aqua // 200
 
-# calculate the amount of ingredients it needs
+
+def qtd_milk(milk):
+    return milk // 50
+
+
+def qtd_beans(coffee):
+    return coffee // 15
+
+
+coffee_machine = []
+
+water_ = int(input('Write how many ml of water the coffee machine has:'))
+coffee_machine.append(qtd_water(water_))
+milk_ = int(input('Write how many ml of milk the coffee machine has:'))
+coffee_machine.append(qtd_milk(milk_))
+coffee_b = int(input('Write how many grams of coffee beans the coffee machine has:'))
+coffee_machine.append(qtd_beans(coffee_b))
+
 cups_coffee = int(input('Write how many cups of coffee you will need:'))
-print(f'For {cups_coffee} cups of coffee you will need:')
-print(f'{cups_coffee * 200} ml of water')
-print(f'{cups_coffee * 50} ml of milk')
-print(f'{cups_coffee * 15} g of coffee beans')
+
+check_coff = map(lambda x: x >= cups_coffee, coffee_machine)
+one_more = min(coffee_machine) - cups_coffee
+
+if all(list(check_coff)):
+    if one_more > 0:
+        print(f' Yes, I can make that amount of coffee ( and even {one_more} more than that)')
+    print("Yes, I can make that amount of coffee")
+else:
+    print(f'No, I can make only {min(coffee_machine)}cups of coffee')
