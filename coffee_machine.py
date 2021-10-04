d16a29b5679be71fd4529cf60901386ec13b0ed5
ton-oliveira <87735990+ton-oliveira@users.noreply.github.com>
2021-10-04 19:16:54 -0300

    Ingredient calculator

--- coffee_machine.py
+++ coffee_machine.py
@@ -7,3 +7,10 @@ Pouring coffee into the cup
 Pouring some milk into the cup
 Coffee is ready!
 ''')
+
+# calculate the amount of ingredients it needs
+cups_coffee = int(input('Write how many cups of coffee you will need:'))
+print(f'For {cups_coffee} cups of coffee you will need:')
+print(f'{cups_coffee * 200} ml of water')
+print(f'{cups_coffee * 50} ml of milk')
+print(f'{cups_coffee * 15} g of coffee beans')
